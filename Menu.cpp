c1625e580d9511f42fa8a09087bed36db850e037
Raezd <31376680+Raezd@users.noreply.github.com>
2022-05-27 23:00:10 -0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -66,18 +66,15 @@ using namespace std;
 
         void Menu::Display() {
 
-            cout << "================================" << endl;
-            cout << "1: Quit" << endl;
-            cout << "2: Destroy List" << endl;
-            cout << "3: Initialize List" << endl;
-            cout << "4: Insert First" << endl;
-            cout << "5: Insert Last" << endl;
-            cout << "6: Insert" << endl;
-            cout << "7: Print" << endl;
-            cout << "8: Reverse Print" << endl;
-            cout << "9: Delete First" << endl;
-            cout << "10: Delete Last" << endl;
-            cout << "11: Delete at Location" << endl;
+            cout << "     ===========================================================" << endl;
+            cout << "     1: Quit             2: Destroy List      3: Initialize List" << endl;
+            cout << endl;
+            cout << "     4: Insert First     5: Insert Last       6: Insert" << endl;
+            cout << endl;
+            cout << "     7: Print            8: Reverse Print     * * * * *"<< endl;
+            cout << endl;
+            cout << "     9: Delete First     10: Delete Last     11: Delete at Location" << endl;
+            cout << "     ==============================================================" << endl;
             cout << endl;
 
         } // Public M-Function Display
@@ -91,49 +88,64 @@ using namespace std;
 
             int selection;
 
-            cout << "Please enter a menu selection: ";
+            cout << "     Please enter a menu selection: ";
             
             cin >> selection;
 
+//          ==================
+//          Switch (selection)
+//          ====================
             switch (selection) {
 
+//              =================================
                 case 1: userMenuSelection = Quit;
                     break;
 
+//              ========================================
                 case 2: userMenuSelection = DestroyList;
                     break;
 
+//              ===========================================
                 case 3: userMenuSelection = InitializeList;
                     break;
 
+//              ========================================
                 case 4: userMenuSelection = InsertFirst;
                     break;
 
+//              =======================================
                 case 5: userMenuSelection = InsertLast;
                     break;
 
+//              ===================================
                 case 6: userMenuSelection = Insert;
                     break;
 
+//              ==================================
                 case 7: userMenuSelection = Print;
                     break;
 
+//              =========================================
                 case 8: userMenuSelection = ReversePrint;
                     break;
 
+//              ========================================
                 case 9: userMenuSelection = DeleteFirst;
                     break;
 
+//              ========================================
                 case 10: userMenuSelection = DeleteLast;
                     break;
 
+//              ====================================
                 case 11: userMenuSelection = Delete;
                     break;
 
+//              ==========================================
                 default: userMenuSelection = ReversePrint;
  
             } // switch (selection)
-//          ==========================
+//          =======================
 
             cout << endl;
 
@@ -157,72 +169,123 @@ using namespace std;
 
         void Menu::ProcessCommand(DoublyLinkedList& list) {
 
+//          =====================
+//          Variable declarations
+//          =====================
             int intValue;
             int location;
+//          =============
 
             if (userMenuSelection != Quit) {
 
+//              ==========================
+//              Switch (userMenuSelection)
 //              ============================
                 switch (userMenuSelection) {
 
+//                  =====================================
                     case DestroyList: list.DestroyList();
                         break;
 
+//                  ===========================================
                     case InitializeList: list.InitializeList();
                         break;
 
+//                  =================
                     case InsertFirst:
 
-                        cout << "Enter a value to add to the beginning of the list: ";
+                        cout << "     Enter a value to add to the beginning of the list: ";
                         cin >> intValue;
                         list.InsertFirst(intValue);
                         list.Print();
                         break;
 
+//                  ================
                     case InsertLast: 
 
-                        cout << "Enter a value to add to the end of the list: ";
+                        cout << "     Enter a value to add to the end of the list: ";
                         cin >> intValue;
                         list.InsertLast(intValue);
                         list.Print();
                         break;
 
+//                  ============
                     case Insert:
 
-                        cout << "Enter a value to add to the list: ";
+                        cout << "     Enter a value to add to the list: ";
                         cin >> intValue;
                         cout << endl;
-                        cout << "Enter a node location for insertion: ";
+                        cout << "     Enter a node location for insertion: ";
                         cin >> location;
                         list.Insert(intValue, location);
                         list.Print();
                         break;
 
+//                  =========================
                     case Print: list.Print();
                         break;
 
-                    case ReversePrint: list.ReversePrint(list.GetFirst());
+                    case ReversePrint: 
+
+                        cout << "     ======================" << endl;
+                        cout << "     Reversed List of Nodes" << endl;
+                        cout << "     ======================" << endl;
+
+                        cout << endl;
+                        cout << "                                   *** Bottom of List ***                             " << endl;
+                        cout << "     =========================================================================" << endl;
+                        cout << "       Prev Address      Current Address       Next Address          Data" << endl;
+                        cout << "     -------------------------------------------------------------------------" << endl;
+                        
+                        list.ReversePrint(list.GetFirst());
+
+                        cout << "     =========================================================================" << endl;
+                        cout << "                                   *** Top of List ***                          " << endl;
+                        cout << endl;
+
                         break;
 
-                    case DeleteFirst: list.DeleteFirst();
+//                  =================
+                    case DeleteFirst: 
+                        
+                        if (list.Length() < 2) {
+
+                            list.DestroyList();
+                            break;
+
+                        } // if
+
+                        list.DeleteFirst();
                         list.Print();
                         break;
 
-                    case DeleteLast: list.DeleteLast();
+//                  ================
+                    case DeleteLast: 
+
+                        if (list.Length() < 2) {
+
+                            list.DestroyList();
+                            break;
+
+                        } // if
+
+                        list.DeleteLast();
                         list.Print();
                         break;
 
+//                  ============
                     case Delete: 
                         
-                        cout << "Enter a node location for deletion: ";
+                        cout << "     Enter a node location for deletion: ";
                         cin >> location;
                         list.Delete(location);
                         list.Print();
                         break;
 
+//                  ========
                     default:
                         
-                        cout << "Invalid selection." << endl;
+                        cout << "     Invalid selection." << endl;
 
                         list.Print();
 
